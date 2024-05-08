15d9fc4e7ce67a562896d423d90cb25a19529787
Nhật Nguyễn Lê Tuấn <22521019@ms.uit.edu.vn>
2024-05-08 08:04:30 +0700

    Add project files.

---
+++ QLBVCB/ViewModel/VM_Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QLBVCB.ViewModel
+{
+    public class VM_Main : VM_Base
+    {
+        public bool IsLoaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public VM_Main()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                IsLoaded = true;
+                Login loginWindow = new Login();
+                loginWindow.ShowDialog();
+            }
+            );
+        }
+    }
+}
