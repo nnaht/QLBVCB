751a510f452a8f52ecc87fbfce3a5ca86bbe6012
dupper15 <nguyenchinghia15@gmail.com>
2024-05-13 15:12:20 +0700

    3:12

---
+++ QLBVCB/ViewModel/VM_ManagePlane.cs
@@ -0,0 +1,29 @@
+﻿using QLBVCB.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QLBVCB.ViewModel
+{
+    internal class VM_ManagePlane : VM_Base
+    {
+        public ICommand OpenAERPlaneCommand { get; set; }
+        public VM_ManagePlane() {
+            OpenAERPlaneCommand = new RelayCommand(ExecuteOpenAERPlaneCommand);
+        }
+        private void ExecuteOpenAERPlaneCommand(object obj)
+        {
+            try
+            {
+                AERPlane aerPlanePage = new AERPlane();
+                aerPlanePage.Show();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
