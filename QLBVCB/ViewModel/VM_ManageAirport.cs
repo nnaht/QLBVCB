3c800586f61d2a8d52c3cd74bbbc95d03a3036ec
dupper15 <nguyenchinghia15@gmail.com>
2024-05-13 21:48:27 +0700

    9:48

--- QLBVCB/ViewModel/VM_ManageAirport.cs
+++ QLBVCB/ViewModel/VM_ManageAirport.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using QLBVCB.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace QLBVCB.ViewModel
 {
     internal class VM_ManageAirport : VM_Base
     {
+        public ICommand OpenAERAirportCommand { get; set; }
+        public VM_ManageAirport()
+        {
+            OpenAERAirportCommand = new RelayCommand(ExecuteOpenAERAirportCommand);
+        }
+        private void ExecuteOpenAERAirportCommand(object obj)
+        {
+            try
+            {
+                AERAirport aERAirport = new AERAirport();
+                aERAirport.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
     }
 }
--- QLBVCB/ViewModel/VM_ManageFlight.cs
+++ QLBVCB/ViewModel/VM_ManageFlight.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using QLBVCB.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace QLBVCB.ViewModel
 {
     internal class VM_ManageFlight : VM_Base
     {
+        public ICommand OpenAERFlightCommand {  get; set; }
+        public VM_ManageFlight() {
+            OpenAERFlightCommand = new RelayCommand(ExecuteOpenAERFlightCommand);
+        }
+        private void ExecuteOpenAERFlightCommand(object obj)
+        {
+            try
+            {
+                AERFlight aERFlight = new AERFlight();
+                aERFlight.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
