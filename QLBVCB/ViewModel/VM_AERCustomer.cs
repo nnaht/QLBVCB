1f567aca921b73fccd789dfa88819e414b0e6b0f
Nhật Nguyễn Lê Tuấn <22521019@ms.uit.edu.vn>
2024-05-16 12:25:06 +0700

    516

---
+++ QLBVCB/ViewModel/VM_AERCustomer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBVCB.ViewModel
+{
+    internal class VM_AERCustomer : VM_Base
+    {
+    }
+}
